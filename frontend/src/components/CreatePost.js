d57e9783f43b0f8d409ac6c2b44305f7467a3892
Karlafgl <karlafgl@gmail.com>
2019-08-17 16:54:20 -0600

    still fixing errors

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -32,7 +32,8 @@ export default class CreatePost extends Component {
         title: this.state.title,
         post_content: this.state.post_content
     };
-      await axios.put('http://localhost:3000/api/post', this.state.id_post, updatedPost)
+      const res = await axios.put('http://localhost:3000/api/post/' + this.state.id_post, updatedPost)
+      console.log(res)
     } else {
       const newPost = {
         title: this.state.title,
