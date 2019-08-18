c2e6392225da1045b5ed6e1a8d3a192088e1b70e
Karlafgl <karlafgl@gmail.com>
2019-08-18 14:28:47 -0600

    adding dinamically date to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 const Footer = () => {
-
-  // Dynamically add copyright year using new Date
-  // document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
-
   return(
     <footer className="bg-light blue">
-      <div id="copyright-year" className="footer-copyright text-center py-3 ">© 2019 Copyright
+      <div id="copyright-year" className="footer-copyright text-center py-3 ">© {(new Date().getFullYear())} Copyright
         <a className="ml-2" href="https://github.com/karlafgl/blog-post-website" target="_blank" rel="noopener noreferrer">Post Website</a>
       </div>
     </footer>
